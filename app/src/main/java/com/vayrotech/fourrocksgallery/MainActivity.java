eb41b23eb57f427c96cf85fc4d56cdddd7a9cc53
Lawrence "Vayro" Biteranta <lbluck@gmail.com>
2021-09-07 20:35:15 -0700

    buttons/fragments

--- app/src/main/java/com/vayrotech/fourrocksgallery/MainActivity.java
+++ app/src/main/java/com/vayrotech/fourrocksgallery/MainActivity.java
@@ -2,7 +2,9 @@ package com.vayrotech.fourrocksgallery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+      toFrameActivity();
+
+
+
+
+
+
+
+
+
+
+
     }
+
+
+
+    //to main frame activity
+    public void toFrameActivity() {
+       // historyString = loadLog();
+        Intent intent = new Intent(this, FrameActivity.class);
+      //  intent.putExtra("log", historyString);
+        //intent.putExtra("filename", FILE_NAME);
+        startActivity(intent);
+    }
+
+
+
+
 }
