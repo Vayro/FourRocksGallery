7c682275872cc416dd11f542da682ebba41a9db4
Lawrence "Vayro" Biteranta <lbluck@gmail.com>
2021-09-12 07:57:50 -0700

    Selected Works!
    Selected Works, take picture with camera and display under selected fragment

--- app/src/main/java/com/vayrotech/fourrocksgallery/FrameActivity.java
+++ app/src/main/java/com/vayrotech/fourrocksgallery/FrameActivity.java
@@ -1,14 +1,19 @@
 package com.vayrotech.fourrocksgallery;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -22,6 +27,8 @@ public class FrameActivity extends AppCompatActivity {
     public static final int CAMERA_REQUEST_CODE = 69;
     public static final int CAMERA_PASS_REQUEST_CODE = 420;
 
+    Bitmap selectedImage; //declare selected image bitmap; this will be passed to the "selected" fragment
+
     //declare buttons
     Button cameraButton, galleryButton, selectedButton;
 
@@ -32,8 +39,9 @@ public class FrameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_frame);
 
 
-
-
+    //initialize bitmap placeholder
+    selectedImage = Bitmap.createBitmap(64, 64, Bitmap.Config.ARGB_8888);
+    selectedImage.eraseColor(android.graphics.Color.RED);
 
 
     //initialize buttons
@@ -78,14 +86,17 @@ public class FrameActivity extends AppCompatActivity {
     galleryButton.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            //stuff
+
+            replaceFragment(new GalleryFragmentActivity());
+
+
         }
     });
 
     selectedButton.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            //stuff
+        replaceFragment(new SelectedFragmentActivity());
         }
     });
 
@@ -146,12 +157,46 @@ public class FrameActivity extends AppCompatActivity {
     }
 
 
-
+    //camera stuff ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     private void openCamera() {
         Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(camera,CAMERA_REQUEST_CODE);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST_CODE) {
+            selectedImage = (Bitmap) data.getExtras().get("data");
+
+
+        }
+
+
+    }
+//end camera stuff ~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+    //load fragment
+    private void replaceFragment(Fragment fragment) {
+
+
+        Bundle bundle = new Bundle();
+        bundle.putParcelable("passedImage", selectedImage);
+        fragment.setArguments(bundle);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
+
+
+
+
+    }
+
+
 
 
 
---
+++ app/src/main/java/com/vayrotech/fourrocksgallery/SelectedFragmentActivity.java
@@ -0,0 +1,109 @@
+package com.vayrotech.fourrocksgallery;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.text.method.ScrollingMovementMethod;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.jetbrains.annotations.NotNull;
+
+
+public class SelectedFragmentActivity extends Fragment {
+
+
+    View view;
+    ImageView selectedImage;
+    Bitmap passedImageFrag;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+
+    public SelectedFragmentActivity() {
+        // Required empty public constructor
+
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ScheduleFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static SelectedFragmentActivity newInstance(String param1, String param2) {
+        SelectedFragmentActivity fragment = new SelectedFragmentActivity();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+      passedImageFrag = this.getArguments().getParcelable("passedImage");
+
+
+
+
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.activity_selected_fragment, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //created stuff goes here
+        selectedImage = getView().findViewById(R.id.selectedView);
+        selectedImage.setImageBitmap(passedImageFrag);
+
+
+    }
+
+
+}
